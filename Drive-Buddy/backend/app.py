11824f82fd2667b8078dfafca2b8284f80ab27e1
aminoxlhd <aminoxlhd@gmail.com>
2024-07-06 20:55:28 +0100

    update get_course and add update_course and delete_course

--- Drive-Buddy/backend/app.py
+++ Drive-Buddy/backend/app.py
@@ -39,10 +39,34 @@ def create_course():
 def get_course(course_id):
     course = courses_collection.find_one({"id": course_id})
     if course:
-        return course['title']
+        return course
     else:
         return {'message': 'Course not found'}, 404
 
+@app.route('/courses/<course_id>', methods=['PUT'])
+def update_course(course_id):
+    course_data = request.get_json()
+    course_schema = CourseSchema()
+
+    try:
+        validated_data = course_schema.load(course_data)
+        updated_course = courses_collection.find_one_and_update(
+            {"id": course_id}, {"$set": validated_data})
+        if updated_course:
+            return {'message': 'Course updated successfully!'}
+        else:
+            return {'message': 'Course not found'}, 404
+    except marshmallow.ValidationError as err:
+        return jsonify({'errors': err.messages}), 400
+
+
+@app.route('/courses/<course_id>', methods=['DELETE'])
+def delete_course(course_id):
+    course = courses_collection.find_one_and_delete({"id": course_id})
+    if course:
+        return {'message': 'Course deleted successfully!'}
+    else:
+        return {'message': 'Course not found'}, 404
 
 
 
