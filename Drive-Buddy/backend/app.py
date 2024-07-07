eaf0262f8882c5dcffc38bb084673a0164fab366
aminoxlhd <aminoxlhd@gmail.com>
2024-07-07 16:51:01 +0100

    Add category

--- Drive-Buddy/backend/app.py
+++ Drive-Buddy/backend/app.py
@@ -3,7 +3,7 @@ import marshmallow
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from flask import Flask, request, jsonify
-from models import  CourseSchema
+from models import  CourseSchema, CategorySchema
 
 app = Flask(__name__)
 
@@ -14,6 +14,7 @@ client = MongoClient(DATABASE_URL, )
 db = client["drive_buddy_db"]
 
 courses_collection = db["courses"]
+category_collection = db["category"]
 instructors_collection = db["instructors"]
 
 
@@ -80,6 +81,68 @@ def get_all_courses():
 
     return jsonify(course_list)
 
+
+@app.route('/category', methods=['GET'])
+def get_all_category():
+    categorys = list(category_collection.find())
+    category_list = []
+    category_schema = CategorySchema()
+
+    for category in categorys:
+        if category:
+            category_dict = category_schema.dump(category)
+            category_list.append(category_dict)
+
+    return jsonify(category_list)
+@app.route('/category', methods=['POST'])
+def create_category():
+    category_data = request.get_json()
+    category_schema = CategorySchema()
+    try:
+        validated_data = category_schema.load(category_data)
+        category_collection.insert_one(validated_data)
+        return {'message': 'Category created successfully!'}
+    except marshmallow.ValidationError as err:
+        return jsonify({'errors': err.messages}), 400
+
+
+@app.route('/category/<category_id>', methods=['GET'])
+def get_category(category_id):
+    category = category_collection.find_one({"id": category_id})
+
+    if category:
+        category_schema = CategorySchema()
+        category_dict = category_schema.dump(category)
+        return category_dict
+
+    else:
+        return {'message': 'Category not found'}, 404
+
+
+@app.route('/category/<category_id>', methods=['PUT'])
+def update_category(category_id):
+    category_data = request.get_json()
+    category_schema = CategorySchema()
+
+    try:
+        validated_data = category_schema.load(category_data)
+        updated_category = category_collection.find_one_and_update(
+            {"id": category_id}, {"$set": validated_data})
+        if updated_category:
+            return {'message': 'Category updated successfully!'}
+        else:
+            return {'message': 'Category not found'}, 404
+    except marshmallow.ValidationError as err:
+        return jsonify({'errors': err.messages}), 400
+
+
+@app.route('/category/<category_id>', methods=['DELETE'])
+def delete_category(category_id):
+    category = category_collection.find_one_and_delete({"id": category_id})
+    if category:
+        return {'message': 'Category deleted successfully!'}
+    else:
+        return {'message': 'Category not found'}, 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Drive-Buddy/backend/models.py
+++ Drive-Buddy/backend/models.py
@@ -6,3 +6,9 @@ class CourseSchema(Schema):
     title = fields.Str()
     category = fields.Str()
     content = fields.Str()
+
+
+class CategorySchema(Schema):
+    id = fields.Str()
+    title = fields.Str()
+    content = fields.Str()
